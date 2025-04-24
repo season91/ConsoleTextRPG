145245de3d7abc47cef18ae9b9559e4b81d1d2b4
yj6662 <yj6662@naver.com>
2025-04-24 11:59:55 +0900

    [feat] BattleSystem
    -feat GameCharacter 직업 추가
    -feat BattleSystem 공격 로직 수정

--- ConsoleTextRPG/GameCharacter.cs
+++ ConsoleTextRPG/GameCharacter.cs
@@ -14,7 +14,7 @@ namespace GameCharacter
             level = 1;
             chad = "전사";
             atk = 10;
-            def = 5;
+            def = 10;
             Mp = 30;
             health = 100;
             gold = 1500;
@@ -31,41 +31,12 @@ namespace GameCharacter
         public override int Attack(Monster target)
         {
             int dmg = target.TakeDamage(atk, this);
-            if (dodgeRate > GameManager.rd.NextDouble())
-            {
-                Mathod.ChangeFontColor(ColorCode.Blue);
-                Console.WriteLine($"→ {target.Name}이 공격을 회피했습니다!");
-                Mathod.ChangeFontColor(ColorCode.None);
-                Thread.Sleep(1000);
-                return 0;
-            }
-            else if (critRate > GameManager.rd.NextDouble())
-            {
-                dmg = (int)(dmg * critDamage);
-                Mathod.ChangeFontColor(ColorCode.Yellow);
-                Console.WriteLine($"→ {target.Name}에게 ({dmg})크리티컬 데미지! (남은 HP {target.monHP}/{target.monMaxHp})");
-                Mathod.ChangeFontColor(ColorCode.None);
-                Thread.Sleep(1000);
-            }
-            else
-            {
-                Mathod.ChangeFontColor(ColorCode.Red);
-                Console.WriteLine($"→ {target.Name}에게 {dmg} 데미지! (남은 HP {target.monHP}/{target.monMaxHp})");
-                Mathod.ChangeFontColor(ColorCode.None);
-                Thread.Sleep(1000);
-
-            }
             return dmg;
         }
         public override int Skill1(Monster target)
         {
             Mp -= Skill1Cost;
             int dmg = target.TakeDamage(atk * 2, this);
-            Mathod.ChangeFontColor(ColorCode.Red);
-            Console.WriteLine($"→ {target.Name}에게 {dmg} 데미지! (남은 HP {target.monHP}/{target.monMaxHp})");
-            Mathod.ChangeFontColor(ColorCode.None);
-            Thread.Sleep(1000);
-
 
             return dmg;
         }
@@ -76,15 +47,11 @@ namespace GameCharacter
 
             for (int i = 0; i < 2; i++)
             {
-                var targetalive = targets.Where(m=>m.IsAlive).ToArray();
+                var targetalive = targets.Where(m => m.IsAlive).ToArray();
                 if (targetalive.Length == 0)
                     break;
                 var target = targetalive[Rdtarget.Next(0, targetalive.Length)];
-                target.TakeDamage(targetDamage, this);
-                Mathod.ChangeFontColor(ColorCode.Red);
-                Console.WriteLine($"→ {target.Name}에게 {targetDamage} 데미지! (남은 HP {target.monHP}/{target.monMaxHp})");
-                Mathod.ChangeFontColor(ColorCode.None);
-                Thread.Sleep(1000);
+                int dmg = target.TakeDamage(targetDamage, this);
             }
             Mp -= Skill2Cost; //스킬 사용 시 MP 소모
         }
@@ -97,7 +64,7 @@ namespace GameCharacter
             level = 1;
             chad = "마법사";
             atk = 10;
-            def = 5;
+            def = 2;
             Mp = 50;
             health = 100;
             gold = 1500;
@@ -113,30 +80,20 @@ namespace GameCharacter
 
         public override int Attack(Monster target)
         {
-            int dmg = target.TakeDamage(atk/10, this);
-            Mathod.ChangeFontColor(ColorCode.Red);
-            Console.WriteLine($"→ {target.Name}에게 {dmg} 데미지! (남은 HP {target.monHP}/{target.monMaxHp})");
-            Mathod.ChangeFontColor(ColorCode.None);
-            Thread.Sleep(1000);
-
+            int dmg = target.TakeDamage(atk, this);
             return dmg;
         }
         public override int Skill1(Monster target)
         {
             Mp -= Skill1Cost;
             int dmg = target.TakeDamage(atk * 3, this);
-            Mathod.ChangeFontColor(ColorCode.Red);
-            Console.WriteLine($"→ {target.Name}에게 {dmg} 데미지! (남은 HP {target.monHP}/{target.monMaxHp})");
-            Mathod.ChangeFontColor(ColorCode.None);
-            Thread.Sleep(1000);
-
 
             return dmg;
         }
         public override void Skill2(Monster[] targets)
         {
             var Rdtarget = GameManager.rd;
-            int targetDamage = (int)(atk * 2.5);
+            int targetDamage = (int)(atk * 1.8);
 
             for (int i = 0; i < 3; i++)
             {
@@ -145,13 +102,111 @@ namespace GameCharacter
                     break;
                 var target = targetalive[Rdtarget.Next(0, targetalive.Length)];
                 int dmg = target.TakeDamage(targetDamage, this);
-                Mathod.ChangeFontColor(ColorCode.Red);
-                Console.WriteLine($"→ {target.Name}에게 {dmg} 데미지! (남은 HP {target.monHP}/{target.monMaxHp})");
-                Mathod.ChangeFontColor(ColorCode.None);
-                Thread.Sleep(1000);
             }
             Mp -= Skill2Cost; //스킬 사용 시 MP 소모
         }
 
     }
+    public class Archer : Job
+    {
+        public Archer()
+        {
+            level = 1;
+            chad = "궁수";
+            atk = 10;
+            def = 3;
+            Mp = 30;
+            health = 100;
+            gold = 1500;
+            exp = 0;
+            critRate = 0.5;
+            critDamage = 2;
+            dodgeRate = 0.1;
+            Skill1Cost = 10;
+            Skill2Cost = 15;
+            Skill1Name = "더블샷";
+            Skill2Name = "멀티샷";
+        }
+
+        public override int Attack(Monster target)
+        {
+            int dmg = target.TakeDamage(atk, this);
+            return dmg;
+        }
+        public override int Skill1(Monster target)
+        {
+            Mp -= Skill1Cost;
+            int dmg = target.TakeDamage(atk * 2, this);
+
+            return dmg;
+        }
+        public override void Skill2(Monster[] targets)
+        {
+            var Rdtarget = GameManager.rd;
+            int targetDamage = (int)(atk * 2.0);
+
+            for (int i = 0; i < 2; i++)
+            {
+                var targetalive = targets.Where(m => m.IsAlive).ToArray();
+                if (targetalive.Length == 0)
+                    break;
+                var target = targetalive[Rdtarget.Next(0, targetalive.Length)];
+                int dmg = target.TakeDamage(targetDamage, this);
+            }
+            Mp -= Skill2Cost; //스킬 사용 시 MP 소모
+        }
+    }
+    public class Berserker : Job
+    {
+        public Berserker()
+        {
+            level = 1;
+            chad = "광전사";
+            atk = 12;
+            def = 5;
+            Mp = 5;
+            health = 150;
+            gold = 1500;
+            exp = 0;
+            critRate = 0.2;
+            critDamage = 2;
+            dodgeRate = 0.1;
+            Skill1HpCost = 3;
+            Skill2HpCost = 5;
+            Skill1Name = "머리쪼개기";
+            Skill2Name = "회전베기";
+        }
+
+        public override int Attack(Monster target)
+        {
+            int dmg = target.TakeDamage(atk, this);
+            return dmg;
+        }
+        public override int Skill1(Monster target)
+        {
+            health -= Skill1HpCost;
+            int dmg = target.TakeDamage(atk * 2, this);
+            Mathod.FontColorOnce($"{Skill1Name} 사용으로 HP{Skill1HpCost} 소모", ColorCode.Red);
+            Thread.Sleep(1000);
+            return dmg;
+        }
+        public override void Skill2(Monster[] targets)
+        {
+            var Rdtarget = GameManager.rd;
+            int targetDamage = atk * 1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                var targetalive = targets.Where(m => m.IsAlive).ToArray();
+                if (targetalive.Length == 0)
+                    break;
+                var target = targetalive[Rdtarget.Next(0, targetalive.Length)];
+                int dmg = target.TakeDamage(targetDamage, this);
+            }
+            health -= Skill2HpCost; //스킬 사용 시 HP 소모
+            Mathod.FontColorOnce($"{Skill2Name} 사용으로 HP{Skill2HpCost} 소모", ColorCode.Red);
+            Thread.Sleep(1000);
+        }
+    }
+
 }
